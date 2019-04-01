dd4f9eaf672b1493b195891596db390ce774e80b
Guillaume EHRET <guillaume@dev-mind.fr>
2019-04-01 08:35:31 +0200

    STEP 2 : generate next empty activities (speaker and speaker list)

---
+++ app/src/main/java/com/devmind/devoxx/SpeakerActivity.kt
@@ -0,0 +1,12 @@
+package com.devmind.devoxx
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SpeakerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_speaker)
+    }
+}
---
+++ app/src/main/java/com/devmind/devoxx/SpeakerListActivity.kt
@@ -0,0 +1,12 @@
+package com.devmind.devoxx
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SpeakerListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_speaker_list)
+    }
+}
